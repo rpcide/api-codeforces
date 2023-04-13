6daa167c232204082adbe301433707a0d4fa35c2
byandrev <byandrev@gmail.com>
2023-04-13 10:56:33 -0500

    add headers

--- scrapper/get_problem.py
+++ scrapper/get_problem.py
@@ -53,7 +53,10 @@ def get_tags(soup: BeautifulSoup) -> List[str]:
 
 
 def get_problem_content(url: str) -> Union[Problem, None]:
-    r = requests.get(url)
+    headers={
+        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0"
+    }
+    r = requests.get(url, headers=headers)
     html = r.content
     soup = BeautifulSoup(html, "html.parser")
     url_splited = r.url.split("/")
